12fd81d5e13646593328f27b608dd1fefbe34c72
Hortense Beaussant <hortense.beaussant@gustaveroussy.fr>
2026-04-06 23:47:30 +0200

    fix error, transmission input_type

--- Cluster/Cluster.py
+++ Cluster/Cluster.py
@@ -70,9 +70,8 @@ def run_tool(args):
             timepoint = float(smpl_spec[4])
 
             patient_data.addSample(maf_fn, sample_id, timepoint_value=timepoint, grid_size=args.grid_size,
-                                   _additional_muts=None,
-                                   seg_file=seg_fn,
-                                   purity=purity)
+                                   _additional_muts=None, seg_file=seg_fn,
+                                   purity=purity, input_type=args.maf_input_type)
             if len(patient_data.sample_list) == args.n_samples:  # use only first N samples
                 break
 
